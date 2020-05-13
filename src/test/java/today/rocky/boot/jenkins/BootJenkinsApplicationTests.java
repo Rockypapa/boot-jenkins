7ef078127bf8f35570a3434fd33566c23e383bee
RockyPapa <2291904124@qq.com>
2020-05-13 12:03:36 +0800

    basic frame

---
+++ src/test/java/today/rocky/boot/jenkins/BootJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package today.rocky.boot.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
